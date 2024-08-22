49e7f2c52df9a7af1f7811c63fb79f83465d0339
tvisha9 <74127814+tvisha9@users.noreply.github.com>
2024-08-21 18:26:02 -0700

    edited package.json and wrote logic for Routes

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,27 +14,29 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) =>{
+  console.log ('POST to Database');
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  await store.put({ content: content,id: Date.now() });
-  await tx.done;
-  console.log('Content added to database:', content);
+  const request= store.put({ content: content,id: Date.now() });
+  const result= await request;
+  console.log('Content added to database:', result);
 }
 
    console.error('putDb not implemented');
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () =>{
+  console.log('Get from database');
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
   const request = store.getAll();
   const data = await request;
-  await tx.done;
+ 
   console.log('Content retrieved from database:', data);
   return data;
   }
- console.error('getDb not implemented');
+
 
 initdb();
