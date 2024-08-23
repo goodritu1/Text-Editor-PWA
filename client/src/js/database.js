5e5b914342c62d38b6974c252e5041c6ea5df986
tvisha9 <74127814+tvisha9@users.noreply.github.com>
2024-08-23 02:53:25 -0700

    finished code for client=>webpack.config

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,30 +13,26 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) =>{
-  console.log ('POST to Database');
-  const jateDb = await openDB('jate', 1);
-  const tx = jateDb.transaction('jate', 'readwrite');
-  const store = tx.objectStore('jate');
-  const request= store.put({ content: content,id: Date.now() });
-  const result= await request;
-  console.log('Content added to database:', result);
-}
-
-   console.error('putDb not implemented');
+export const putDb = async (content) => {console.log('put in DATABASE');
+const jateDb = await openDB('jate', 1);
+const tx = jateDb.transaction('jate', 'readwrite');
+const store = tx.objectStore('jate');
+const request = store.put({ id: 1, value: content });
+const result = await request;
+console.log('🚀 - data saved to the database', result.value);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () =>{
-  console.log('Get from database');
-  const jateDb = await openDB('jate', 1);
-  const tx = jateDb.transaction('jate', 'readonly');
-  const store = tx.objectStore('jate');
-  const request = store.getAll();
-  const data = await request;
- 
-  console.log('Content retrieved from database:', data);
-  return data;
-  }
+export const getDb = async () => {console.log('get from DATABASE');
+
+const jateDb = await openDB('jate', 1);
+const tx = jateDb.transaction('jate', 'readonly');
+const store = tx.objectStore('jate');
+const request = store.get(1);
+const result = await request;
+console.log('result.value', result);
+return result?.value;
+};
 
 
 initdb();
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -4,8 +4,6 @@ const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
-
-
 // TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
@@ -20,32 +18,40 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
+      // Webpack plugin that generates our html file and injects our bundles.
       new HtmlWebpackPlugin({
         template: './index.html',
-        title: 'TODOs List'
+        title: 'JATE'
       }),
-
-      new GenerateSW(),
+      // Injects our custom service worker
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+      // Creates a manifest.json file.
       new WebpackPwaManifest({
-        name: 'TODOs',
-        short_name: 'TODOs',
-        description: 'Keep track of important tasks!',
-        background_color: '#7eb4e2',
-        theme_color: '#7eb4e2',
+        fingerprints: false,
+        inject: true,
+        name: 'Just Another Text Editor',
+        short_name: 'JATE',
+        description: 'Takes notes with JavaScript syntax highlighting!',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        crossorigin: 'use-credentials', 
         start_url: './',
         publicPath: './',
         icons: [
           {
-            src: path.resolve('assets/images/logo.png'),
+            src: path.resolve('src/images/logo.png'),
             sizes: [96, 128, 192, 256, 384, 512],
             destination: path.join('assets', 'icons'),
           },
         ],
       }),
-     
     ],
 
     module: {
+      // CSS loaders
       rules: [
         {
           test: /\.css$/i,
@@ -54,7 +60,7 @@ module.exports = () => {
         {
           test: /\.m?js$/,
           exclude: /node_modules/,
-       
+          // Babel-loader in order to use ES6.
           use: {
             loader: 'babel-loader',
             options: {
@@ -63,7 +69,11 @@ module.exports = () => {
             },
           },
         },
+
       ],
     },
   };
 };
+
+
+
